458b5a55eee247966dbca6187a64a43d9533402c
Zi Wang <ziwang@uber.com>
2016-01-12 19:10:51 -0800

    00, test Java console project with IntelliJ

---
+++ 00_TestJavaConsoleIntelliJ/src/Main.java
@@ -0,0 +1,14 @@
+import com.zi.test.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Person p = new Person();
+        p.setName("Zee");
+        p.setAge(29);
+
+        System.out.println(p.getName());
+    }
+}
